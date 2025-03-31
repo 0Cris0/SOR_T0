42a382313abf583b59cf5e6a4aeaf0b1c434ed87
0Cris <cristbal.albornoz@uc.cl>
2025-03-31 19:12:34 -0300

    Avance lun31 19:12

--- Tests/hello.c
+++ Tests/hello.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
 int main(int argc, char const *argv[]){
+    printf("Soy el programa/proceso de Hello\n");
+    // Los argumentos válidos parten desde el 1, porque el 0 es la dirección
+    printf("Argumentos = {%d} 1: [%s] 2:[%s]\n", argc, argv[1], argv[2]);
+    sleep(3);
     printf("AAAAAAAAA\n");
+    printf("-----+++------\n");
     return 0;
 }
--- src/dccAdmin/main.c
+++ src/dccAdmin/main.c
@@ -1,11 +1,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "../input_manager/manager.h"
 // TODO: Entender qué hace eso
@@ -42,22 +43,23 @@ typedef struct proceso {
 
 
 
-void agregar_proceso(struct proceso* procesos, char name[256], pid_t pid){
-  if(procesos == NULL){
+void agregar_proceso(struct proceso* procesos, char* name, pid_t pid){
+  /* printf("++++++++ procesos puntero = %p\n", procesos);
+  printf("++++++++ procesos PID = %d\n", procesos->pid); */
+  if(procesos->pid == 0){
     // Testear que se cumpla
     strcpy(procesos->nombre, name);
-    printf(">>>> testeando nombre %s v/s copiado %s\n", name, procesos->nombre);
     procesos->anterior = NULL;
     procesos->siguiente = NULL;
     procesos->exit_code = -1;
     procesos->pid = pid;
     procesos->tiempo_inicio = time(NULL);
     procesos->tiempo_final = 0;
+  printf("Ha quedado el head con PID = %d\n", procesos->pid);
   }
   else{
     struct proceso* nuevo_proceso = calloc(1, sizeof(struct proceso));
     strcpy(nuevo_proceso->nombre, name);
-    printf(">>>> testeando nombre %s v/s copiado %s\n", name, nuevo_proceso->nombre);
     nuevo_proceso->anterior = NULL;
     nuevo_proceso->siguiente = NULL;
     nuevo_proceso->exit_code = -1;
@@ -71,6 +73,7 @@ void agregar_proceso(struct proceso* procesos, char name[256], pid_t pid){
     }
     nuevo_proceso->anterior = proceso_actual;
     proceso_actual->siguiente = nuevo_proceso;
+  printf("Ha agregado proceso con PID = %d\n", nuevo_proceso->pid);
   }
 }
 
@@ -85,91 +88,124 @@ void agregar_proceso(struct proceso* procesos, char name[256], pid_t pid){
  void liberar_procesos(struct proceso* procesos){
   struct proceso* proceso_actual = procesos;
   struct proceso* proceso_a_liberar;
-  printf("== Liberando procesos\n");
+  printf("\n== Liberando procesos\n");
+  printf("=======================\n");
   while(proceso_actual != NULL){
     proceso_a_liberar = proceso_actual;
     proceso_actual = proceso_actual->siguiente;
-    printf("=== Liberando proceso con PID = {%d}\n", proceso_a_liberar->pid);
+    printf("--- Liberando proceso con PID = {%d}\n", proceso_a_liberar->pid);
     free(proceso_a_liberar);
   }
+  printf("=======================\n");
  }
 
 
 
 
 
+void start(char** input, struct proceso* procesos){
+  if(input[1] != NULL){
+    pid_t pid = fork();
+    switch (pid) {
+      case -1:
+          perror("[Error]: Error en el fork\n");
+          exit(EXIT_FAILURE);
+      case 0:
+          // Hijo;
+          int cantidad_args = 0;
+          for(int i=1; input[i]!=NULL; i++){
+            cantidad_args+=1;
+          }
+          char** copia_args = calloc(cantidad_args+1, sizeof(char*));
+          // Ahora recorro los argumentos del input copiándolos y agrego NULL al final
+          //printf("Argumentos entregados:\n");
 
- void start(const char *executable_path, char *const argv[], struct proceso* procesos){
-  /* Tomar ruta del executable y sus argumentos y los ejecuta
-   con nu nuevo proceso
-  Caso 1: Executable no existe
-    Indicar error al usuario
-  Caso 2: Executable existe
-    Hacer fork para crear nuevo proceso desde la shell
-    Hacer execve?? para poder aplicar el código del executable al proceso
-
-    No debo congelar la shell
-    */
-
-  pid_t pid = fork();
-  if(pid < 0){
-    perror("[Error]: Ha sucedido un error con fork()");
+          for(int j=1; input[j]!=NULL; j++){ //TODO: Considerar volver j=2 para saltarme eel path
+            copia_args[j-1] = strdup(input[j]);
+            //printf("- %s\n", copia_args[j-1]);
+          }
+          copia_args[cantidad_args] = NULL;
+          // Ahora aplico execve
+          execve(input[1], copia_args, NULL);
+          perror("[Error]: Execve ha fallado, por favor revisa que el ejecutable exista y que los argumentos entregados sean correctos\n");
+          for(int k=0; k<cantidad_args; k++){
+            free(copia_args[k]);
+          }
+          free(copia_args);
+          
+          //-------
+          // Debo agregar manera de que termine
+      default:
+          // Padre
+          agregar_proceso(procesos, input[1], pid);
+          int status;
+          pid_t resultado = waitpid(pid, &status, WNOHANG); //TODO: ver si esto funciona
+          // Debo agregar manera de que espere al hijo
+          if(resultado == 0){
+            printf("> Hijo aún en ejecución, esperando...\n");
+          }
+          else{
+            printf("> Hijo ya terminó, continuando ejecución\n");
+          }
+      }
   }
-  // Agregar en los casos de abajo manera de agregar a la LL de procesos iniciados por dccAdmin
-  else if (pid == 0) {
-    printf("Soy el hijo con PID %d\n", getpid());
-    printf("Soy el hijo con PID %d\n", getpid());
-    // Intentar ejecutar execve y ver si tira error
-    printf("Ejecutando excecve\n");
-    execve(executable_path, argv, NULL);
-    perror("[Error]: Ha ocurrido un error al ejecutar execv, puede que el ejecutable no exista\n");
-  } else if (pid > 0) {
-    printf("Soy el padre con PID %d y mi hijo tiene PID %d\n", getpid(), pid);
-    int status;
-    pid_t resultado = waitpid(pid, &status, WNOHANG);
-    if(resultado == 0){
-      printf("Padre ha encontrado al hijo aún en ejecución, esperando...\n");
-    } else {
-      printf("Padre, hijo ha terminado\n");
-    }
-    
+  else{
+    perror("[Error]: Por favor entrega un path del ejecutable y argumentos válidos\n");
   }
+  
 
 }
-void info(){
-  /* Debo printear listado de todos los programas ejecutados desde
-   dccAdmin y que esten siendo ejecutados en ese entonces
-      PID
-      Nombre del ejecutable
-      Tiempo de ejecución en segundos
-      Exit code
-        -1 si ell proceso hijo NO ha terminado
-        exit code si hijo ha terminado
-      Signal value recibida por el proceso
-        -1 si no recibió señal
-        Valor de la señal recibida por proceso
-          Depende del tipo de señal que emita
-   */
-}
+
+
+
 
 
 
 int main(int argc, char const *argv[])
 {
-  char** input = read_user_input();
-  printf("%s\n", input[0]);
-  perror("EEEEERORR TEST");
-  free_user_input(input);
-
-
+  bool consola_activa = true;
+  printf("=== Bienvenido/a a DCC-Admin ===\n");
+  printf("=== ------<--------->------- ===\n");
   struct proceso* procesos = calloc(1, sizeof(struct proceso));
-  // Hacr análisis de casos según command
-  // Caso start
-    // Aquí creo proceso y lo agrego a la LL
-  // Caso info
-  // Caso timeout
-  // Caso quit
-  liberar_procesos(procesos);
+
+  while(true){
+    if(!consola_activa){
+      // Cerrar consola
+      liberar_procesos(procesos);
+      printf("[>] Cerrando consola [<]\n");
+      exit(0);
+      break;
+    }
+
+    
+    printf("[>] Por favor, ingrese un comando:\n");
+    char** input = read_user_input();
+    printf("$ %s\n", input[0]);
+
+    // Casos de input
+    if(strcmp(input[0], "exit")==0){
+      // Liberar procesos y memoria van aquí
+      consola_activa = false;
+    }
+    else if(strcmp(input[0], "start")==0){
+      start(input, procesos);
+    }
+    else if(strcmp(input[0], "info")==0){
+      //
+    }
+    else if(strcmp(input[0], "timeout")==0){
+      //
+    }
+    else if(strcmp(input[0], "quit")==0){
+      //
+      // Liberar procesos y memoria van aquí
+      consola_activa = false;
+    }
+    free_user_input(input);
+
+    
+
+  } 
   return 0;
 }
 
